534c7d49cb97b96f0ed542e6ea7859520a3a333b
Matt Favoino <mattfavoino@gmail.com>
2017-08-24 15:57:38 -0700

    added constructor function to js and listener

---
+++ app_matt.js
@@ -0,0 +1,31 @@
+'use strict';
+
+function Order(name, street, city, state, zip, phone, cc, product, qty) {
+  this.name = name;
+  this.street = street;
+  this.city = city;
+  this.state = state;
+  this.zip = zip;
+  this.phone = phone;
+  this.cc = cc;
+  this.product = product;
+  this.qty;
+};
+
+function submitOrder(event) {
+  event.preventDefault();
+  var name = form.elements['name'].value;
+  var street = form.elements['street'].value;
+  var city = form.elements['city'].value;
+  var state = form.elements['state'].value;
+  var zip = form.elements['zip'].value;
+  var phone = form.elements['phone'].value;
+  var cc = form.elements['cc'].value;
+  var product = form.elements['product'].value;
+  var qty = form.elements['qty'].value;
+  var newEntry = new Order(name, street, city, state, zip, phone, cc, product, qty);
+  form.reset();
+};
+
+var form = document.getElementById('form');
+form.addEventListener('subimt', submitOrder);
